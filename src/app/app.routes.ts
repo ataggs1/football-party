65b128e3656b314c10cbdc617793f13fea9f1860
Ann Taggart <Ann.Taggart@hyland.com>
2024-09-06 13:44:09 -0400

    added home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProfessionalGamesComponent } from './professional-games/professional-games.component';
 import { CollegeGamesComponent } from './college-games/college-games.component';
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
-
+  {path: 'home', component: HomeComponent},
   { path: 'professional-games', component: ProfessionalGamesComponent },
   { path: 'college-games', component: CollegeGamesComponent },
-  { path: '', redirectTo: '/professional-games', pathMatch: 'full' } // Default route
+  { path: '', redirectTo: '/home', pathMatch: 'full' } // Default route
 ];
